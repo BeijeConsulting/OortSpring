46ee2baefc6f54ad7a2774375ed95f04d9892a21
ivfly <iv0fly@gmail.com>
2020-10-12 17:39:43 +0200

    Spring Data Jpa

--- src/main/java/it/beije/oort/controller/HomeController.java
+++ src/main/java/it/beije/oort/controller/HomeController.java
@@ -1,12 +1,14 @@
 package it.beije.oort.controller;
 
 import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Locale;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,10 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import it.beije.oort.entity.Utente;
+import it.beije.oort.service.UtenteService;
 
 
 @Controller
 public class HomeController {
+	
+	@Autowired
+	private UtenteService utenteService;
 	
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home() {
@@ -68,11 +74,29 @@ public class HomeController {
 		System.out.println("utente : " + id);
 		
 		//carico utente...
-		Utente utente = new Utente();
-		utente.setCognome("Zegna");
-		utente.setNome("Ermenegildo");
+//		Utente utente = new Utente();
+//		utente.setCognome("Zegna");
+//		utente.setNome("Ermenegildo");
+		
+		Utente utente = utenteService.load(id);
+		
+		//String nominativo = utenteService.getNominativo(utente);
 		
 		model.addAttribute("utente", utente);
+		//model.addAttribute("nominativo", nominativo);
+		
+		return "utente";
+	}
+
+	@RequestMapping(value = "/utente/nome/{nome}", method = RequestMethod.GET)
+	public String utente(@PathVariable String nome, Model model) {
+		System.out.println("utente : " + nome);
+		
+		List<Utente> utenti = utenteService.findByNome(nome);
+		
+		System.out.println(utenti);
+		
+		model.addAttribute("utente", utenti.size() > 0 ? utenti.get(0) : new Utente() );
 		
 		return "utente";
 	}
