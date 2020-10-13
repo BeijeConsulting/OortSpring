0b7ced6378bd6664c20b8eeba1c907ca16c987cb
Brando <b.mancuso37@gmail.com>
2020-10-13 11:19:14 +0200

    repo conf 2

--- src/main/java/it/beije/oort/bm/library/repository/UserRepository.java
+++ src/main/java/it/beije/oort/bm/library/repository/UserRepository.java
@@ -1,8 +1,11 @@
 package it.beije.oort.bm.library.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import it.beije.oort.bm.library.entity.User;
+
 @Repository
-public class UserRepository {
+public interface UserRepository extends JpaRepository<User, Integer>{
 
 }
