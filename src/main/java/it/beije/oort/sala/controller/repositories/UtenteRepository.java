54771e53b8729f3784f0d5cee9e027d032cf5178
h4rk <ale.sala.97@gmail.com>
2020-10-13 10:50:10 +0200

    initialized UtenteRepository

---
+++ src/main/java/it/beije/oort/sala/controller/repositories/UtenteRepository.java
@@ -0,0 +1,20 @@
+package it.beije.oort.sala.controller.repositories;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import it.beije.oort.entity.Utente;
+
+@Repository
+public interface UtenteRepository extends JpaRepository<Utente, Integer> {
+	
+	Optional<Utente> findByEmailAndPassword(String email, String password);
+	
+	Optional<Utente> findById(Integer id);
+
+	List<Utente> findByNomeLike(String nome);
+	
+}
