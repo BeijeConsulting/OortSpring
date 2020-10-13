dffedf5944ad631df972e964a86bad7f28ee06c9
Brando <b.mancuso37@gmail.com>
2020-10-13 10:36:13 +0200

    commmit pre switch

---
+++ src/main/java/it/beije/oort/bm/library/database/JpaEntityManager.java
@@ -0,0 +1,11 @@
+package it.beije.oort.bm.library.database;
+
+import javax.persistence.EntityManagerFactory;
+
+public class JpaEntityManager {
+
+	public static EntityManagerFactory getInstance() {
+		return null;
+	}
+
+}
