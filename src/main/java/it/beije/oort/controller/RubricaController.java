3c8bf7ecfe5c1d213df7ae077941ede00b3a54e3
Padawan10 <Padawan10@LAPTOP-G8D3LA7T.homenet.telecomitalia.it>
2020-10-20 14:54:12 +0200

    old

--- src/main/java/it/beije/oort/controller/RubricaController.java
+++ src/main/java/it/beije/oort/controller/RubricaController.java
@@ -1,5 +1,6 @@
 package it.beije.oort.controller;
 import org.slf4j.Logger;
+
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
