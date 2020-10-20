967770a2f92d1c6d69559002f109bd9ca4ede9fe
ivfly <iv0fly@gmail.com>
2020-10-20 14:56:44 +0200

    REST

--- src/main/java/it/beije/oort/controller/HomeController.java
+++ src/main/java/it/beije/oort/controller/HomeController.java
@@ -62,41 +62,41 @@ public class HomeController {
 		return "form_utente";
 	}
 
-	@RequestMapping(value = "/utente", method = RequestMethod.POST)
-	public String utente(Utente utente, HttpServletRequest request, Model model) {//@RequestParam String cognome
-		System.out.println("ricevo dati utente...");
-		
-//		Utente utente = new Utente();
-//		utente.setCognome(cognome);
-//		utente.setNome(request.getParameter("nome"));
-//		utente.setEmail(request.getParameter("email"));
-//		utente.setTelefono(request.getParameter("telefono"));
-		
-		utenteService.insert(utente);
-		
-		model.addAttribute("utente", utente);
-		
-		return "utente";
-	}
-
-	@RequestMapping(value = "/utente/{id}", method = RequestMethod.GET)
-	public String utente(@PathVariable Integer id, Model model) {
-		System.out.println("utente : " + id);
-		
-		//carico utente...
-//		Utente utente = new Utente();
-//		utente.setCognome("Zegna");
-//		utente.setNome("Ermenegildo");
-		
-		Utente utente = utenteService.load(id);
-		
-		//String nominativo = utenteService.getNominativo(utente);
-		
-		model.addAttribute("utente", utente);
-		//model.addAttribute("nominativo", nominativo);
-		
-		return "utente";
-	}
+//	@RequestMapping(value = "/utente", method = RequestMethod.POST)
+//	public String utente(Utente utente, HttpServletRequest request, Model model) {//@RequestParam String cognome
+//		System.out.println("ricevo dati utente...");
+//		
+////		Utente utente = new Utente();
+////		utente.setCognome(cognome);
+////		utente.setNome(request.getParameter("nome"));
+////		utente.setEmail(request.getParameter("email"));
+////		utente.setTelefono(request.getParameter("telefono"));
+//		
+//		utenteService.insert(utente);
+//		
+//		model.addAttribute("utente", utente);
+//		
+//		return "utente";
+//	}
+//
+//	@RequestMapping(value = "/utente/{id}", method = RequestMethod.GET)
+//	public String utente(@PathVariable Integer id, Model model) {
+//		System.out.println("utente : " + id);
+//		
+//		//carico utente...
+////		Utente utente = new Utente();
+////		utente.setCognome("Zegna");
+////		utente.setNome("Ermenegildo");
+//		
+//		Utente utente = utenteService.load(id);
+//		
+//		//String nominativo = utenteService.getNominativo(utente);
+//		
+//		model.addAttribute("utente", utente);
+//		//model.addAttribute("nominativo", nominativo);
+//		
+//		return "utente";
+//	}
 
 	@RequestMapping(value = "/utente/nome/{nome}", method = RequestMethod.GET)
 	public String utente(@PathVariable String nome, Model model) {
